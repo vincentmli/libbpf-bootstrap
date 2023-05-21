f0760a4f754867ea7fe09ea8487c082be6316fc5
Vincent Li <vincent.mc.li@gmail.com>
2023-05-20 18:00:48 -0700

    remove type define

--- examples/c/siphash4bpf.c
+++ examples/c/siphash4bpf.c
@@ -19,23 +19,23 @@
 #define cROUNDS 2
 #define dROUNDS 4
 
-#define ROTL(x, b) (uint64_t)(((x) << (b)) | ((x) >> (64 - (b))))
+#define ROTL(x, b) (__u64)(((x) << (b)) | ((x) >> (64 - (b))))
 
 #define U32TO8_LE(p, v)                                                        \
-    (p)[0] = (uint8_t)((v));                                                   \
-    (p)[1] = (uint8_t)((v) >> 8);                                              \
-    (p)[2] = (uint8_t)((v) >> 16);                                             \
-    (p)[3] = (uint8_t)((v) >> 24);
+    (p)[0] = (__u8)((v));                                                   \
+    (p)[1] = (__u8)((v) >> 8);                                              \
+    (p)[2] = (__u8)((v) >> 16);                                             \
+    (p)[3] = (__u8)((v) >> 24);
 
 #define U64TO8_LE(p, v)                                                        \
-    U32TO8_LE((p), (uint32_t)((v)));                                           \
-    U32TO8_LE((p) + 4, (uint32_t)((v) >> 32));
+    U32TO8_LE((p), (__u32)((v)));                                           \
+    U32TO8_LE((p) + 4, (__u32)((v) >> 32));
 
 #define U8TO64_LE(p)                                                           \
-    (((uint64_t)((p)[0])) | ((uint64_t)((p)[1]) << 8) |                        \
-     ((uint64_t)((p)[2]) << 16) | ((uint64_t)((p)[3]) << 24) |                 \
-     ((uint64_t)((p)[4]) << 32) | ((uint64_t)((p)[5]) << 40) |                 \
-     ((uint64_t)((p)[6]) << 48) | ((uint64_t)((p)[7]) << 56))
+    (((__u64)((p)[0])) | ((__u64)((p)[1]) << 8) |                        \
+     ((__u64)((p)[2]) << 16) | ((__u64)((p)[3]) << 24) |                 \
+     ((__u64)((p)[4]) << 32) | ((__u64)((p)[5]) << 40) |                 \
+     ((__u64)((p)[6]) << 48) | ((__u64)((p)[7]) << 56))
 
 #define SIPROUND                                                               \
     do {                                                                       \
@@ -57,10 +57,10 @@
 
 #ifdef DEBUG
 #define TRACE                                                           \
-        bpf_printk("v0 %x %x\n", (v0 >> 32), (uint32_t)v0); \
-        bpf_printk("v1 %x %x\n", (v1 >> 32), (uint32_t)v1); \
-        bpf_printk("v2 %x %x\n", (v2 >> 32), (uint32_t)v2); \
-        bpf_printk("v3 %x %x\n", (v3 >> 32), (uint32_t)v3);
+        bpf_printk("v0 %x %x\n", (v0 >> 32), (__u32)v0); \
+        bpf_printk("v1 %x %x\n", (v1 >> 32), (__u32)v1); \
+        bpf_printk("v2 %x %x\n", (v2 >> 32), (__u32)v2); \
+        bpf_printk("v3 %x %x\n", (v3 >> 32), (__u32)v3);
 #else
 #define TRACE
 #endif
@@ -73,39 +73,39 @@
 #define HEXTOBYTE(c) (HEXTONIBBLE(c)*16 + HEXTONIBBLE(c+1))
 
 #define COOKIE_SECRET_K0 \
-    ( ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+ 0) <<  0) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+ 2) <<  8) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+ 4) << 16) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+ 6) << 24) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+ 8) << 32) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+10) << 40) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+12) << 48) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+14) << 56))
+    ( ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+ 0) <<  0) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+ 2) <<  8) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+ 4) << 16) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+ 6) << 24) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+ 8) << 32) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+10) << 40) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+12) << 48) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+14) << 56))
 
 #define COOKIE_SECRET_K1 \
-    ( ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+16) <<  0) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+18) <<  8) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+20) << 16) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+22) << 24) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+24) << 32) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+26) << 40) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+28) << 48) \
-    | ((uint64_t)HEXTOBYTE(COOKIE_SECRET_STR+30) << 56))
+    ( ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+16) <<  0) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+18) <<  8) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+20) << 16) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+22) << 24) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+24) << 32) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+26) << 40) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+28) << 48) \
+    | ((__u64)HEXTOBYTE(COOKIE_SECRET_STR+30) << 56))
 
 #define INLENv4 20
 #define INLENv6 32
 #define OUTLEN   8
-static inline void siphash_ipv4(const uint8_t *in, uint8_t *out)
+static inline void siphash_ipv4(const __u8 *in, __u8 *out)
 {
-    uint64_t v0 = 0x736f6d6570736575ULL ^ COOKIE_SECRET_K0;
-    uint64_t v1 = 0x646f72616e646f6dULL ^ COOKIE_SECRET_K1;
-    uint64_t v2 = 0x6c7967656e657261ULL ^ COOKIE_SECRET_K0;
-    uint64_t v3 = 0x7465646279746573ULL ^ COOKIE_SECRET_K1;
-    uint64_t m;
+    __u64 v0 = 0x736f6d6570736575ULL ^ COOKIE_SECRET_K0;
+    __u64 v1 = 0x646f72616e646f6dULL ^ COOKIE_SECRET_K1;
+    __u64 v2 = 0x6c7967656e657261ULL ^ COOKIE_SECRET_K0;
+    __u64 v3 = 0x7465646279746573ULL ^ COOKIE_SECRET_K1;
+    __u64 m;
     int i;
-    const uint8_t *end = in + INLENv4 - (INLENv4 % sizeof(uint64_t));
+    const __u8 *end = in + INLENv4 - (INLENv4 % sizeof(__u64));
     const int left = INLENv4 & 7;
-    uint64_t b = ((uint64_t)INLENv4) << 56;
+    __u64 b = ((__u64)INLENv4) << 56;
     if (OUTLEN == 16)
         v1 ^= 0xee;
 
@@ -122,19 +122,19 @@ static inline void siphash_ipv4(const uint8_t *in, uint8_t *out)
 
     switch (left) {
     case 7:
-        b |= ((uint64_t)in[6]) << 48;
+        b |= ((__u64)in[6]) << 48;
     case 6:
-        b |= ((uint64_t)in[5]) << 40;
+        b |= ((__u64)in[5]) << 40;
     case 5:
-        b |= ((uint64_t)in[4]) << 32;
+        b |= ((__u64)in[4]) << 32;
     case 4:
-        b |= ((uint64_t)in[3]) << 24;
+        b |= ((__u64)in[3]) << 24;
     case 3:
-        b |= ((uint64_t)in[2]) << 16;
+        b |= ((__u64)in[2]) << 16;
     case 2:
-        b |= ((uint64_t)in[1]) << 8;
+        b |= ((__u64)in[1]) << 8;
     case 1:
-        b |= ((uint64_t)in[0]);
+        b |= ((__u64)in[0]);
         break;
     case 0:
         break;
@@ -161,17 +161,17 @@ static inline void siphash_ipv4(const uint8_t *in, uint8_t *out)
     U64TO8_LE(out, b);
 }
 
-static inline void siphash_ipv6(const uint8_t *in, uint8_t *out)
+static inline void siphash_ipv6(const __u8 *in, __u8 *out)
 {
-    uint64_t v0 = 0x736f6d6570736575ULL ^ COOKIE_SECRET_K0;
-    uint64_t v1 = 0x646f72616e646f6dULL ^ COOKIE_SECRET_K1;
-    uint64_t v2 = 0x6c7967656e657261ULL ^ COOKIE_SECRET_K0;
-    uint64_t v3 = 0x7465646279746573ULL ^ COOKIE_SECRET_K1;
-    uint64_t m;
+    __u64 v0 = 0x736f6d6570736575ULL ^ COOKIE_SECRET_K0;
+    __u64 v1 = 0x646f72616e646f6dULL ^ COOKIE_SECRET_K1;
+    __u64 v2 = 0x6c7967656e657261ULL ^ COOKIE_SECRET_K0;
+    __u64 v3 = 0x7465646279746573ULL ^ COOKIE_SECRET_K1;
+    __u64 m;
     int i;
-    const uint8_t *end = in + INLENv6 - (INLENv6 % sizeof(uint64_t));
+    const __u8 *end = in + INLENv6 - (INLENv6 % sizeof(__u64));
     const int left = INLENv6 & 7;
-    uint64_t b = ((uint64_t)INLENv6) << 56;
+    __u64 b = ((__u64)INLENv6) << 56;
     if (OUTLEN == 16)
         v1 ^= 0xee;
 
@@ -188,19 +188,19 @@ static inline void siphash_ipv6(const uint8_t *in, uint8_t *out)
 
     switch (left) {
     case 7:
-        b |= ((uint64_t)in[6]) << 48;
+        b |= ((__u64)in[6]) << 48;
     case 6:
-        b |= ((uint64_t)in[5]) << 40;
+        b |= ((__u64)in[5]) << 40;
     case 5:
-        b |= ((uint64_t)in[4]) << 32;
+        b |= ((__u64)in[4]) << 32;
     case 4:
-        b |= ((uint64_t)in[3]) << 24;
+        b |= ((__u64)in[3]) << 24;
     case 3:
-        b |= ((uint64_t)in[2]) << 16;
+        b |= ((__u64)in[2]) << 16;
     case 2:
-        b |= ((uint64_t)in[1]) << 8;
+        b |= ((__u64)in[1]) << 8;
     case 1:
-        b |= ((uint64_t)in[0]);
+        b |= ((__u64)in[0]);
         break;
     case 0:
         break;
