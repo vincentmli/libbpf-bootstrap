01323533f76f933d3bdd5b082c1db27f4eda97e2
Vincent Li <vincent.mc.li@gmail.com>
2023-09-30 18:09:01 +0000

    Run iptables rule and XDP program in one script

--- examples/c/xdp-iptables.sh
+++ examples/c/xdp-iptables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-interface=$1
+interface=eno2
+ports="80,8080"
 sysctl -w net.ipv4.tcp_syncookies=2
 sysctl -w net.ipv4.tcp_timestamps=1
 sysctl -w net.netfilter.nf_conntrack_tcp_loose=0
@@ -9,4 +10,6 @@ iptables -t filter -A INPUT -i $interface -p tcp -m tcp --dport 80 -m state --st
 iptables -t raw -I PREROUTING  -i $interface -p tcp -m tcp --syn --dport 8080 -j CT --notrack
 iptables -t filter -A INPUT -i $interface -p tcp -m tcp --dport 8080 -m state --state INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
 iptables -t filter -A INPUT -i $interface -m state --state INVALID -j DROP
+
+/usr/local/bin/xdppacket --iface $interface --mss4 1460 --mss6 1440 --wscale 7 --ttl 254 --ports $ports
 
